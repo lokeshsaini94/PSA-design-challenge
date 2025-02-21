1fd49fb5d90ef28fe0404ef9db6c9d6d673a7b3f
Lokesh <lokeshsaini94@gmail.com>
2025-02-21 00:51:25 -0800

    - Unversioned files

---
+++ lib/core/constant/util.dart
@@ -0,0 +1,153 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:psa_task/core/theme/colors_psa.dart';
+
+const List<String> pokemonCardsImages = [
+  'assets/images/card1.png',
+  'assets/images/card2.png',
+  'assets/images/card3.png',
+  'assets/images/card4.png',
+  'assets/images/card1.png',
+  'assets/images/card2.png',
+  'assets/images/card3.png',
+  'assets/images/card4.png',
+];
+
+const List<String> pokemonCards = [
+  "1999 Pokemon Game 1st Edition Charizard - Holo #4",
+  "1999 Pokemon Game 1st Edition  Mewtwo #10",
+  "1999 Pokemon Game 1st Edition  Mewtwo #10",
+  "1999 Pokemon Game 1st Edition  Blastoise #2",
+  "1999 Pokemon Game 1st Edition Charizard - Holo #4",
+  "1999 Pokemon Game 1st Edition  Mewtwo #10",
+  "1999 Pokemon Game 1st Edition  Mewtwo #10",
+  "1999 Pokemon Game 1st Edition  Blastoise #2",
+];
+
+List<String> pokemonCardsValue = [
+  "\$3,565.17",
+  "\$293.99",
+  "\$63.31",
+  "\$531.70",
+  "\$3,565.17",
+  "\$293.99",
+  "\$63.31",
+  "\$531.70",
+];
+
+const List<double> pokemonCardsValueChange = [
+  26.5,
+  2.7,
+  1.2,
+  7.22,
+  6.5,
+  2.7,
+  1.2,
+  7.2,
+];
+
+const List<IconData> pokemonCardsValueChangeIcons = [
+  Icons.arrow_upward,
+  Icons.arrow_downward,
+  Icons.arrow_downward,
+  Icons.arrow_upward,
+  Icons.arrow_upward,
+  Icons.arrow_downward,
+  Icons.arrow_downward,
+  Icons.arrow_upward,
+];
+
+const List<Color> pokemonCardsValueChangeColors = [
+  ColorsPSA.textSuccess,
+  ColorsPSA.textError,
+  ColorsPSA.textError,
+  ColorsPSA.textSuccess,
+  ColorsPSA.textSuccess,
+  ColorsPSA.textError,
+  ColorsPSA.textError,
+  ColorsPSA.textSuccess,
+];
+List<FlSpot> spots1 = [
+  FlSpot(0, 2),
+  FlSpot(2, 3),
+  FlSpot(4, 1),
+  FlSpot(4.8, 2.5),
+  FlSpot(7, 3),
+  FlSpot(9, 1.5),
+  FlSpot(10, 2.5),
+];
+List<FlSpot> spots2 = [
+  FlSpot(0, 3),
+  FlSpot(2, 1),
+  FlSpot(4, 2.5),
+  FlSpot(4.8, 1.5),
+  FlSpot(7, 3),
+  FlSpot(9, 1),
+  FlSpot(10, 2),
+];
+
+List<String> imagePaths = [
+  'assets/images/profile1.png',
+  'assets/images/profile2.png',
+  'assets/images/profile3.png',
+  'assets/images/profile4.png',
+  'assets/images/profile5.png',
+  'assets/images/profile6.png',
+  'assets/images/profile7.png',
+  'assets/images/profile8.png',
+];
+
+List<String> names = [
+  'Steph',
+  'Kristina',
+  'Harry',
+  'Lionel',
+  'Annina',
+  'Tarra',
+  'Molly',
+  'Kathy',
+];
+
+List<String> status = [
+  'PSA 9',
+  'Raw',
+  'PSA 4',
+  'Raw',
+  'Raw',
+  'Raw',
+  'Raw',
+  'Raw',
+];
+List<String> grades = [
+  'UNGRADED',
+  'PSA 10',
+  'PSA 9',
+  'PSA 8',
+  'PSA 7',
+  'PSA 6',
+];
+
+List<String> price = [
+  '\$3,565.90',
+  '\$319,999',
+  '\$17,500',
+  '\$7,592.69',
+  '\$6,500.00',
+  '\$5,650.00',
+];
+
+List<String> subHead = [
+  'Current value',
+  'Pop 124',
+  'Pop 718',
+  'Pop 648',
+  'Pop 516',
+  'Pop 705',
+];
+
+const List<String> pokemonCardsValueChangeStrings = [
+  "26.5%",
+  "2.7%",
+  "1.2%",
+  "7.2%",
+];
---
+++ lib/util/scroll_to_hide_widget.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ScrollToHideWidget extends StatefulWidget {
+  final Widget child;
+  final ScrollController controller;
+  final Duration duration;
+
+  const ScrollToHideWidget({
+    required this.child,
+    required this.controller,
+    this.duration = const Duration(milliseconds: 200),
+    super.key,
+  });
+
+  @override
+  State<ScrollToHideWidget> createState() => _ScrollToHideWidgetState();
+}
+
+class _ScrollToHideWidgetState extends State<ScrollToHideWidget> {
+  bool isVisible = true;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(listen);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(listen);
+    super.dispose();
+  }
+
+  void listen() {
+    final direction = widget.controller.position.userScrollDirection;
+    if (direction == ScrollDirection.forward) {
+      show();
+    } else if (direction == ScrollDirection.reverse) {
+      hide();
+    }
+  }
+
+  void show() {
+    if (!isVisible) setState(() => isVisible = true);
+  }
+
+  void hide() {
+    if (isVisible) setState(() => isVisible = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: widget.duration,
+      height: isVisible ? (Platform.isIOS ? 96 : 72) : 0,
+      padding: isVisible ? EdgeInsets.zero : EdgeInsets.only(top: 40),
+      child: Wrap(children: [widget.child]),
+    );
+  }
+}
